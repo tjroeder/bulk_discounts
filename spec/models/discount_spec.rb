a8a4532d72c8314dcc7d1e0094d69c6808be6607
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 23:45:31 -0700

    Feat: add merchant validation

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Discount, type: :model do
   end
   
   describe 'validations' do
+    it { should validate_presence_of :merchant }
     it { should validate_presence_of :percent }
     it { should validate_presence_of :threshold }
     it do 
