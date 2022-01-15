8d356f709632b593fabf0d22111d42c9388ef47f
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 21:28:35 -0700

    Fix: change to merchant_items controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :merchants do
     resources :dashboard, only: [:index], controller: :merchant_dashboard
     resources :discounts, only: [:index, :show], controller: :merchant_discounts
-    resources :items
+    resources :items, controller: :merchant_items
     resources :invoices, only: [:index, :show]
     resources :invoice_items, only: [:update]
   end
