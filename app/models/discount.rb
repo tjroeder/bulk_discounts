832988160975c1f995149dcb0d3e4b54c44e7edd
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 23:46:12 -0700

    Feat: add merchant validation

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,6 +3,7 @@ class Discount < ApplicationRecord
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
 
+  validates_presence_of :merchant
   validates_presence_of :percent, :threshold
   validates_numericality_of :percent, greater_than: 0, less_than: 100, only_integer: true
   validates_numericality_of :threshold, greater_than: 0, only_integer: true
