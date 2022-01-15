f5947d4acc3661426d95a3cfc885bda420cdf0f9
Tim Roeder <timothyjroeder@gmail.com>
2022-01-15 15:38:08 -0700

    Feat: create #destoy action, and #find_merchant callback

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,6 +1,7 @@
 class MerchantDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new, :create, :destroy]
+  
   def index
-    @merch = Merchant.find(params[:merchant_id])
     @discs = @merch.discounts
   end
 
@@ -9,24 +10,32 @@ class MerchantDiscountsController < ApplicationController
   end
 
   def new
-    @merch = Merchant.find(params[:merchant_id])
   end
-
+  
   def create
-    merch = Merchant.find(discount_params[:merchant_id])
-    discount = merch.discounts.new(discount_params[:discount])
-
+    discount = @merch.discounts.new(discount_params[:discount])
+    
     if discount.save 
-      redirect_to merchant_discounts_path(merch)
+      redirect_to merchant_discounts_path(@merch)
     else
-      redirect_to new_merchant_discount_path(merch)
+      redirect_to new_merchant_discount_path(@merch)
       flash[:alert] = "Error: #{error_message(discount.errors)}"
     end
+  end
+  
+  def destroy
+    Discount.find(discount_params[:id]).destroy
+
+    redirect_to merchant_discounts_path(@merch)
   end
 
   private
 
+  def find_merchant
+    @merch = Merchant.find(params.permit(:merchant_id)[:merchant_id])
+  end
+
   def discount_params
-    params.permit(:merchant_id, discount: [:percent, :threshold])
+    params.permit(:merchant_id, :id, discount: [:percent, :threshold])
   end
 end
