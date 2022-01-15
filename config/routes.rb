6500a17daf8870001ad60dc73c7f7f602a90bb33
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 21:36:40 -0700

    Fix: route to merchant_invoices controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :dashboard, only: [:index], controller: :merchant_dashboard
     resources :discounts, only: [:index, :show], controller: :merchant_discounts
     resources :items, controller: :merchant_items
-    resources :invoices, only: [:index, :show]
+    resources :invoices, only: [:index, :show], controller: :merchant_invoices
     resources :invoice_items, only: [:update]
   end
 
