ede85d09dcdd977da27e668406d528ef71a7b5e2
Tim Roeder <timothyjroeder@gmail.com>
2022-01-14 22:17:47 -0700

    Fix: resources changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,7 @@ Rails.application.routes.draw do
 
   resources :admin, only: [:index]
   namespace :admin do
-    resources :merchants do
-    end
-    resources :merchants, only: [:index, :show, :edit, :update]
+    resources :merchants, only: [:index, :show, :new, :create, :edit, :update]
     resources :invoices, only: [:index, :show, :update]
   end
 end
