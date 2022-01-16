38dbffca8ef7d03dd62a7763bf8f23b870ec24f2
Tim Roeder <timothyjroeder@gmail.com>
2022-01-16 10:10:32 -0700

    Test: create ::total_revenue tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -14,10 +14,16 @@ RSpec.describe InvoiceItem, type: :model do
 
   describe 'instance methods' do
     describe '#total_revenue' do
-      it 'returns quantity multiplied by unit price' do
-        invoice_items = create_list(:invoice_item, 3, quantity: 2, unit_price: 100)
-  
-        expect(InvoiceItem.total_revenue).to eq(600)
+      it 'returns quantity multiplied by unit price for each invoice item' do
+        invoice_item_1 = create(:invoice_item, quantity: 4, unit_price: 500)
+        invoice_items = create_list(:invoice_item, 2, quantity: 2, unit_price: 100)
+        invoice_item_2 = create(:invoice_item, quantity: 5, unit_price: 200)
+        examples = InvoiceItem.total_revenue.order(revenue: :desc)
+
+        expect(examples[0].revenue).to eq(2000)
+        expect(examples[1].revenue).to eq(1000)
+        expect(examples[2].revenue).to eq(200)
+        expect(examples[3].revenue).to eq(200)
       end
     end
   end
