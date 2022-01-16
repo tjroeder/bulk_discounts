f3a11bb6b693b162cacf3120baaa9647d795b971
Tim Roeder <timothyjroeder@gmail.com>
2022-01-15 22:33:35 -0700

    Test: add ::successful scope test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe Transaction, type: :model do
   describe 'relationships' do
     it { should belong_to(:invoice) }
@@ -9,5 +8,17 @@ RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { should validate_presence_of :credit_card_number }
     it { should define_enum_for(:result).with(['default', 'failed', 'success']) }
+  end
+
+  describe 'class methods' do
+    describe '::successful' do
+      it 'returns all successful transactions' do
+        transaction_successes = create_list(:transaction, 2, :success)
+        no_success = create(:transaction)
+
+        expect(Transaction.successful).to contain_exactly(transaction_successes[0], transaction_successes[1])
+        expect(Transaction.successful).not_to include(no_success)
+      end
+    end
   end
 end
