7fc5e1351d6616680cb487beb0a260d473dc7dcb
Tim Roeder <timothyjroeder@gmail.com>
2022-01-16 23:16:00 -0700

    Feat: create relationships, #best_discount and #potential_revenue

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,13 +1,24 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+  has_one :merchant, through: :item
+  has_many :discounts, through: :merchant
+
   enum status: ['pending', 'packaged', 'shipped']
 
-  validates :quantity, presence: true
-  validates :unit_price, presence: true
+  validates_presence_of :quantity, :unit_price, :status, :invoice_id, :item_id
 
   # Class methods
   scope :total_revenue, -> { select('SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue').group(:id) }
 
   scope :not_shipped, -> { where('invoice_items.status != ?', 2) }
+
+  # Instance Methods
+  def best_discount
+    discounts.order(percent: :desc).where('threshold <= :quantity', quantity: self.quantity).first
+  end
+
+  def potential_revenue
+    unit_price * quantity
+  end
 end
