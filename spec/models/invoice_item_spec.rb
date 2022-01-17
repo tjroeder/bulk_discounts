ecd753ee5a937963afd0f68cf53fe167a9b2f06b
Tim Roeder <timothyjroeder@gmail.com>
2022-01-17 13:55:45 -0700

    Test: add additional relationship and validation tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to(:item) }
     it { should belong_to(:invoice) }
+    it { should have_one(:merchant).through(:item) }
+    it { should have_many(:discounts).through(:merchant) }
   end
 
   describe 'validations' do
-    it {should validate_presence_of :quantity}
-    it {should validate_presence_of :unit_price}
+    it { should validate_presence_of :quantity }
+    it { should validate_presence_of :unit_price }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :invoice_id }
+    it { should validate_presence_of :item_id }
     it { should define_enum_for(:status).with_values(['pending', 'packaged', 'shipped']) }
   end
 
