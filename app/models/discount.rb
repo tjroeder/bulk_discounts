98c14d8f1bbd7d7f970add764cb9901299460413
Tim Roeder <timothyjroeder@gmail.com>
2022-01-18 17:49:51 -0700

    Feat: add has_many invoices relationship

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,6 +2,7 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
 
   validates_presence_of :merchant
   validates_presence_of :percent, :threshold
