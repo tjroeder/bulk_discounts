b660c62fa69258310897953a20f685637a24e391
Tim Roeder <timothyjroeder@gmail.com>
2022-01-18 21:14:43 -0700

    Feat: update numerous methods for #discounted_amount addition

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -33,15 +33,26 @@ class Invoice < ApplicationRecord
     invoice_items.where('invoice_items.status = ?', 1)
   end
 
-  def ii_filtered_by_merch(merch_id)
-    invoice_items.joins(:merchant).where(merchants: { id: merch_id })
+  def ii_filtered_by_merch(merch = nil)
+    return invoice_items.joins(:merchant).where(merchants: { id: merch }) if merch
+    invoice_items
   end
 
-  def pre_discount_revenue(merch_id)
-    ii_filtered_by_merch(merch_id).sum('invoice_items.unit_price * invoice_items.quantity')
+  def pre_discount_revenue(merch = nil)
+    ii_filtered_by_merch(merch).sum('invoice_items.unit_price * invoice_items.quantity')
   end
 
-  def discounted_revenue(merch_id)
-    ii_filtered_by_merch(merch_id).sum(&:line_item_revenue)
+  def discounted_amount(merch = nil)
+    Invoice.select('SUM(discount_rev.discounted_ii) AS discount_sum')
+           .from(self.ii_filtered_by_merch(merch)
+                     .joins(:discounts)
+                     .where('invoice_items.quantity >= discounts.threshold')
+                     .select('invoice_items.id, max(invoice_items.unit_price * invoice_items.quantity * discounts.percent / 100.0) AS discounted_ii')
+                     .group('invoice_items.id'), :discount_rev)
   end
+
+  def total_discounted_revenue(merch = nil)
+    pre_discount_revenue(merch) - discounted_amount(merch)[0]['discount_sum'].to_f
+  end 
 end
+
